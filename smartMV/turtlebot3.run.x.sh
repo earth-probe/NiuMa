d8495e4f8d87f9a7f90735daa235e0b736a6809f
Wator Vapor <watorvapor@gmail.com>
2021-12-27 21:48:19 +0900

    navi2

---
+++ smartMV/turtlebot3.run.x.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+pwd=`pwd`
+cpus_num=$(grep processor /proc/cpuinfo | wc -l)
+echo ${cpus_num}
+cpustart=$((cpus_num / 2))
+cpusend=$((cpus_num -1 ))
+echo ${cpustart}
+echo ${cpusend}
+docker run \
+  -u $(id -u ${USER}):$(id -g ${USER}) \
+  --group-add dialout \
+  --group-add audio \
+  --group-add video \
+  -v ${PWD}:${PWD} \
+  -v /etc/passwd:/etc/passwd \
+  -v /etc/group:/etc/group \
+  -v ${HOME}:${HOME} \
+  --privileged \
+  --workdir="${PWD}"\
+  --net=host \
+  --cpuset-cpus=${cpustart}-${cpusend} \
+  -it niuma_navi2 /bin/bash 
