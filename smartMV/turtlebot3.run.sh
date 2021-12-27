8fdcfdaff22ebea6cf1657c4685dd0da45ce1e96
Wator Vapor <watorvapor@gmail.com>
2021-12-27 21:57:49 +0900

    Update turtlebot3.run.sh

--- smartMV/turtlebot3.run.sh
+++ smartMV/turtlebot3.run.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-pwd=`pwd`
-cpus_num=$(grep processor /proc/cpuinfo | wc -l)
-echo ${cpus_num}
-cpustart=$((cpus_num / 2))
-cpusend=$((cpus_num -1 ))
-echo ${cpustart}
-echo ${cpusend}
-docker run \
-  -u $(id -u ${USER}):$(id -g ${USER}) \
-  --group-add dialout \
-  --group-add audio \
-  --group-add video \
-  -v ${PWD}:${PWD} \
-  -v /etc/passwd:/etc/passwd \
-  -v /etc/group:/etc/group \
-  -v ${HOME}:${HOME} \
-  --privileged \
-  --workdir="${PWD}"\
-  --net=host \
-  --cpuset-cpus=${cpustart}-${cpusend} \
-  -it niuma_navi2 ros2 launch  turtlebot3_gazebo turtlebot3_world.launch.py
+source /opt/ros/<ros2-distro>/setup.bash
